2e3b3ce9b810099bb0b88f40b389293ef509b543
Reece Hanham <rhanham93@gmail.com>
2014-03-25 03:07:49 +0000

    Added support for output streams such as std::cout.

--- Vector.h
+++ Vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <iostream>
+
 class Vector
 {
 public:
@@ -10,6 +12,16 @@ public:
 public:
     static Vector add(Vector const& lhs, Vector const& rhs);
     static Vector sub(Vector const& lhs, Vector const& rhs);
+
+    inline friend std::ostream& operator << (std::ostream& out, Vector const& vec)
+    {
+        out << '(' << vec.x_ << ',';
+        out << vec.y_ << ',';
+        out << vec.z_ << ',';
+        out << vec.w_ << ')';
+
+        return out;
+    }
 
 public:
     float x_;
