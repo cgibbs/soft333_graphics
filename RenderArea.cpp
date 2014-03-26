b17a3702bf061527bcb294269a2f660b8d27c487
Reece Hanham <rhanham93@gmail.com>
2014-03-26 01:56:14 +0000

    RenderArea drawing simple square model.

--- RenderArea.cpp
+++ RenderArea.cpp
@@ -1,5 +1,9 @@
 #include "RenderArea.h"
 
+#include <QPen>
+#include <QBrush>
+#include <QPainter>
+
 RenderArea::RenderArea(QWidget *parent) :
     QWidget(parent)
 {
@@ -9,9 +13,119 @@ RenderArea::RenderArea(QWidget *parent) :
     this->setBackgroundRole(QPalette::Base);
     this->setPalette(pal);
     this->setAutoFillBackground(true);
+
+    // Set matrices as identity.
+    modelMatrix_ = Matrix::identity();
+    viewMatrix_ = Matrix::identity();
+    perspectiveMatrix_ = Matrix::identity();
+
+    // Initialise the default model.
+    std::vector<Vertex> vertices;
+    vertices.push_back({-100.0f, -100.0f, 0.0f});
+    vertices.push_back({-100.0f, 100.0f, 0.0f});
+    vertices.push_back({100.0f, -100.0f, 0.0f});
+    vertices.push_back({100.0f, 100.0f, 0.0f});
+
+    std::vector<int> indexes;
+    indexes.push_back(0); indexes.push_back(1); indexes.push_back(2);
+    indexes.push_back(1); indexes.push_back(2); indexes.push_back(3);
+
+    renderModel_ = Model(vertices, indexes);
 }
 
-void RenderArea::paintEvent(QPaintEvent *event)
+void RenderArea::setModelMatrix(const Matrix &modelMat)
 {
+    modelMatrix_ = modelMat;
+}
+
+void RenderArea::setViewMatrix(const Matrix &viewMat)
+{
+    viewMatrix_ = viewMat;
+}
+
+void RenderArea::setPerspectiveMatrix(const Matrix &perspectiveMat)
+{
+    perspectiveMatrix_ = perspectiveMat;
+}
+
+void RenderArea::paintEvent(QPaintEvent * /*event*/)
+{
+    // Iterate through each triangle in the model.
+    std::size_t numTriangles = renderModel_.getIndexes().size() / 3;
+
+    std::cout << "RenderArea::paintEvent(): " << std::endl;
+
+
+    // Draw the background.
+    QPainter painter(this);
+    painter.setPen(palette().dark().color());
+    painter.setBrush(Qt::NoBrush);
+    painter.drawRect(0, 0, width() -1, height() -1);
+
+    for(std::size_t t = 0; t < numTriangles; t++) {
+
+        // Allocate memory for the triangle.
+        std::vector<Vertex> triangle;
+        triangle.resize(3);
+
+        // Iterate through each vertex.
+        for(size_t i = 0; i < 3; i++) {
+
+            std::size_t index = renderModel_.getIndexes()[i + t*3];
+
+            Vector vert;
+            vert.x_ = renderModel_.getVertices()[index].x;
+            vert.y_ = renderModel_.getVertices()[index].y;
+            vert.z_ = renderModel_.getVertices()[index].z;
+            vert.w_ = 1.0f;
+
+            // Transform the vertex.
+            vert = Matrix::multiplyColumnVector(modelMatrix_, vert);
+            vert = Matrix::multiplyColumnVector(viewMatrix_, vert);
+            vert = Matrix::multiplyColumnVector(perspectiveMatrix_, vert);
+
+            // Copy the vertex into the triangle vertex array.
+            triangle[i].x = vert.x_;
+            triangle[i].y = vert.y_;
+            triangle[i].z = vert.z_;
+
+        }
+
+        drawTriangle(triangle, painter);
+
+
+    }
+
+}
+
+void RenderArea::drawTriangle(const std::vector<Vertex> &triangle, QPainter& painter)
+{
+    std::cout << triangle[0].x << ' ' << triangle[0].y << std::endl;
+    std::cout << triangle[1].x << ' ' << triangle[1].y << std::endl;
+    std::cout << triangle[2].x << ' ' << triangle[2].y << std::endl;
+
+    QPointF const points[3] = {
+        QPointF(triangle[0].x, triangle[0].y),
+        QPointF(triangle[1].x, triangle[1].y),
+        QPointF(triangle[2].x, triangle[2].y)
+    };
+
+    painter.save();
+
+    QPen pen;
+    pen.setColor(QColor(200, 50, 50));
+    pen.setWidth(2);
+
+    QBrush brush;
+    brush.setColor(QColor(150,150,150));
+    brush.setStyle(Qt::SolidPattern);
+
+    painter.setBrush(brush);
+    painter.setPen(pen);
+
+    painter.drawPolygon(points, 3);
+
+    painter.restore();
+
 
 }
--- RenderArea.h
+++ RenderArea.h
@@ -3,19 +3,53 @@
 
 #include <QWidget>
 
+#include "Matrix.h"
+#include "Vector.h"
+#include "Model.h"
+
 class RenderArea : public QWidget
 {
     Q_OBJECT
 public:
     explicit RenderArea(QWidget *parent = 0);
 
+public:
+    /**
+     * @brief setModelMatrix
+     * @param modelMat
+     */
+    void setModelMatrix(Matrix const& modelMat);
+
+    /**
+     * @brief setViewMatrix
+     * @param viewMat
+     */
+    void setViewMatrix(Matrix const& viewMat);
+
+    /**
+     * @brief setPerspectiveMatrix
+     * @param perspectiveMat
+     */
+    void setPerspectiveMatrix(Matrix const& perspectiveMat);
+
 private:
     void paintEvent(QPaintEvent *event) override;
 
-signals:
+    /**
+     * @brief drawTriangle Render a single triangle to the screen.
+     * @param triangle Vertex array containing the vertices of the triangle in screen space.
+     */
+    void drawTriangle(std::vector<Vertex> const& triangle, QPainter& painter);
 
-public slots:
+private:
 
+    // Transformation matrices.
+    Matrix modelMatrix_;
+    Matrix viewMatrix_;
+    Matrix perspectiveMatrix_;
+
+    // Model to render.
+    Model renderModel_;
 };
 
 #endif // RENDERAREA_H
